c8a3cceecdd3b25a3d038d8f927a97dac01b99a5
samruddhikokare <160456416+samruddhikokare@users.noreply.github.com>
2024-10-28 11:17:09 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const taskRoutes = require('./routes/taskRoutes');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/tasks', taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
